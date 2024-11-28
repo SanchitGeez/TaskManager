3915c2dd6cf45ae03582537c777c3268c41db8b3
SanchitGeez <sanchit3546@gmail.com>
2024-11-28 12:57:49 +0530

    vercel config

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -7,6 +7,12 @@ const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
+
+// Home Route
+app.get("/", (req, res) => {
+    res.send("serverhome");
+  });
+
 app.use("/api/tasks", taskRoutes);
 
 export default app;
